2adef9a5e18d99768fef0ea3e3271a16f8559068
Faiz Alaam <faizalaam@Faizs-MacBook-Pro.local>
2021-12-14 13:20:13 +0530

    added ideathon popup

--- script.js
+++ script.js
@@ -40,3 +40,55 @@ function myFunction() {
       x.style.display = "block";
     }
 }
+
+//ideathon popup
+$(document).ready(function() {	
+
+  var id = '#dialog';
+    
+  //Get the screen height and width
+  var maskHeight = $(document).height();
+  var maskWidth = $(window).width();
+    
+  //Set heigth and width to mask to fill up the whole screen
+  $('#mask').css({'width':maskWidth,'height':maskHeight});
+  
+  //transition effect
+  $('#mask').fadeIn(500);	
+  $('#mask').fadeTo("slow",0.9);	
+    
+  //Get the window height and width
+  var winH = $(window).height();
+  var winW = $(window).width();
+                
+  //Set the popup window to center
+  $(id).css('top', '50%');
+  $(id).css('left', '50%');
+  $(id).css('transform', 'translate(-50%,-50%);');
+    
+  //transition effect
+  $(id).fadeIn(2000); 	
+    
+  //if close button is clicked
+  $('.window .close').click(function (e) {
+  //Cancel the link behavior
+  e.preventDefault();
+  
+  $('#mask').hide();
+  $('.window').hide();
+  });
+  
+  //if mask is clicked
+  $('#mask').click(function () {
+  $(this).hide();
+  $('.window').hide();
+  });
+    
+  });
+
+
+
+
+
+
+
